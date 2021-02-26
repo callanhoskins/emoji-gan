6d760d02f6360f09507d02131ba47f78ac09d0d5
Callan Hoskins <callanhoskins@gmail.com>
2021-02-25 21:06:01 -0800

    Convert image preprocessing to python file

--- image_preprocess.py
+++ image_preprocess.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[15]:
 
 
 import cv2
@@ -10,10 +10,15 @@ import matplotlib.pyplot as plt
 import os
 import face_recognition
 
-# get_ipython().run_line_magic('matplotlib', 'inline')
+# In[16]:
 
 
-# In[2]:
+# DEFINE CONSTANTS
+SIZE = (128, 128)
+SAVE_DIRECTORY = 'resized_emoji_challenge_128_faces'
+
+
+# In[17]:
 
 
 files_paths = glob.glob('emoji_challenge/*/*.jpg')
@@ -21,17 +26,13 @@ print(f'Total: {len(files_paths)}')
 files_paths = [file for file in files_paths if 'problematic' not in file]
 print(f'Excluding problematic: {len(files_paths)}')
 
-
-# In[23]:
+EXCLUDE_DIRS = ['eye_roll', 'here_we_go', 'surprise', 'angry']
+files_paths = [file for file in files_paths if not any(x in file for x in EXCLUDE_DIRS)]
 
 
 for folder in os.listdir('emoji_challenge'):
     if os.path.isdir(os.path.join('emoji_challenge', folder)):
         print(f'{folder}: {len(os.listdir(os.path.join("emoji_challenge", folder)))}')
-
-
-# ## Cropping and padding
-# In[9]:
 
 
 # This function crops the image to be square with its dimensions min(height, width) x min(height, width)
@@ -42,7 +43,7 @@ def crop_img(img):
     return img
 
 
-# In[10]:
+# In[21]:
 
 
 # This function zero-pads the image on the top+bottom/left+right so that it's a square
@@ -55,6 +56,10 @@ def zero_pad_img(img):
         pad_amt = (height - width) // 2
         img = cv2.copyMakeBorder(img, 0, 0, pad_amt, pad_amt, cv2.BORDER_CONSTANT, value=0)
     return img
+
+# ## Remove sketched emojies
+# Many of the images are in the shape of a square and have a picture of a person on the left and a picture of a hand-drawn emoji on the right. We don't care about the emoji on the right, so we want to remove it. 
+
 
 
 # Returns the proportion of an image's height to its width
@@ -75,17 +80,45 @@ def tall_image(img):
     else: 
         return img
 
-if not os.path.exists('resized_emoji_challenge_128'):
-    os.makedirs('resized_emoji_challenge_128')
+
+# Returns tuple of location of the largest face in the image
+def find_face_loc(locations):
+    max_area = 0
+    max_loc = (0, 0, 0, 0)
+    for tup in locations: 
+        top, right, bottom, left = tup
+        area = (bottom - top) * (right - left)
+        max_area = area if area > max_area else max_area
+        max_loc = tup if area == max_area else max_loc
+    return max_loc
+
+
+# In[31]:
+
+
+# Returns cropped image of largest face in the photo (or uncropped image if no face found)
+def crop_face(img): 
+    locations = face_recognition.face_locations(img)
+    face_loc = find_face_loc(locations)
+    if face_loc == (0, 0, 0, 0): 
+        return None
+    top, right, bottom, left = face_loc
+    return img[top:bottom, left:right, :]
+
+
+if not os.path.exists(SAVE_DIRECTORY):
+    os.makedirs(SAVE_DIRECTORY)
 for file in files_paths:
     relative_path = os.path.join(*(file.split(os.path.sep)[1:]))
+    print(relative_path)
     folder, filename = os.path.split(relative_path)
-    if not os.path.exists(os.path.join('resized_emoji_challenge_128', folder)):
-        os.makedirs(os.path.join('resized_emoji_challenge_128', folder))
+    if not os.path.exists(os.path.join(SAVE_DIRECTORY, folder)):
+        os.makedirs(os.path.join(SAVE_DIRECTORY, folder))
     image = cv2.imread(file)
     image = tall_image(image)
-    if not len(face_recognition.face_locations(image)):
+    image = crop_face(image)
+    if image is None:
         continue
     image = zero_pad_img(image)
-    image = cv2.resize(image, (128, 128))
-    cv2.imwrite(os.path.join('resized_emoji_challenge_128/', folder, filename), image)
+    image = cv2.resize(image, SIZE)
+    cv2.imwrite(os.path.join(f'{SAVE_DIRECTORY}/', folder, filename), image)
